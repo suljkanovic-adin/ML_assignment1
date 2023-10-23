9b9e27e0bd8182125ef616aa684fd269fc23ad24
suljkanovic-adin <adin.suljkanovic.001@student.uni.lu>
2023-10-23 19:43:34 +0200

    kmeans_iris done

--- C.1/kmeans_iris.py
+++ C.1/kmeans_iris.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import silhouette_samples, silhouette_score
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -47,5 +48,53 @@ print(f"Accuracy: {accuracy:.2f}")
 conf_matrix = confusion_matrix(data['species'], data['MappedCluster'])
 print("Confusion Matrix:")
 print(conf_matrix)
+
+#4.
+silhouette_vals = silhouette_samples(X, data['Cluster'])
+data['Silhouette'] = silhouette_vals
+
+avg_silhouette_scores = data.groupby('Cluster')['Silhouette'].mean()
+
+print("Average Silhouette Scores for every cluster:")
+print(avg_silhouette_scores)
+
+overall_avg_silhouette = silhouette_score(X, data['Cluster'])
+print(f"\nOverall Average Silhouette Score: {overall_avg_silhouette:.2f}")
+
+#5
+unknown_data = pd.read_csv("./unknown_species.csv")
+
+#lenght features are in columns 1 to 4
+X_unknown = unknown_data.iloc[:, 1:5].values
+
+unknown_data['PredictionCluster'] = kmeans.predict(X_unknown)
+
+silhouette_scores = silhouette_samples(X_unknown, unknown_data['PredictionCluster'])
+
+unknown_data['Silhouette'] = silhouette_scores
+
+#printing the outcome of prediction clusters
+print("Here are the prediction clusters for uknown species:")
+print(unknown_data[['id', 'PredictionCluster']].to_string(index=False))
+
+#printing the silhouette scores of different flowers
+#printing the outcome
+print("Here are the silhouette scores for uknown species:")
+print(unknown_data[['id', 'Silhouette']].to_string(index=False))
+
+#interpretation of the silhouette scores
+for index, row in unknown_data.iterrows():
+    print(f"\nFlower ID {row['id']}:")
+    if row['Silhouette'] > 0.6:
+        print("This flower is well clustered.")
+    elif row['Silhouette'] > 0.3:
+        print("This flower is moderately clustered, may have some overlap or closeness with another cluster.")
+    else:
+        print("This flower might be close to the boundary of another cluster and maybe better suited to a neighboring cluster.")
+
+
+
+
+
 
 
